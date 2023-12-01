723e0f9900bd3804bc8b4e253dceab23ad9fdafa
khaled <lsnoop75@gmail.com>
2023-12-01 20:11:36 +0100

    Queue with linked list just the start

--- Queue/linked-list/main.c
+++ Queue/linked-list/main.c
@@ -1,4 +1,40 @@
 #include <stdio.h> 
+#include <stdlib.h>
+
+typedef int QueueEntry;
+
+typedef struct queuenode{
+     QueueEntry entry;
+     struct queuenode *next;
+} QueueNode;
+
+typedef struct queue{
+    QueueNode *front;
+    QueueNode *rear;
+    int size;
+} Queue;
+
+void CreateQueue(Queue *pq)
+{
+    pq->font=NULL;
+    pq->rear=NUll;
+    pq->size=0;
+}
+
+void Append(QueueEntry e, Queue *pq)
+{
+    QueueNode *pn = (QueueNode*) malloc(sizeof(QueueNode));
+    pn->entry = e;
+    pn->next = NULL;
+    if(!pq->front)
+        pq->front = pn;
+    else
+        pq->rear->next = pn;
+    pq->rear = pn;  
+    pq->size++;
+}
+
+
 
 int main() 
 {
